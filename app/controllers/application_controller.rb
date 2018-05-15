ae6cd321ac9ebbf79e89afc6fcc7297cd0570491
LeeSD90 <lee.doyle90@gmail.com>
2018-05-15 10:57:00 +0200

    Fix redirect loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    # Devise override
+    def after_sign_in_path_for(resource)
+        edit_user_registration_path
+    end
+
+    # Devise override
+    def after_sign_out_path_for(resource)
+        new_user_session_path
+    end
 end

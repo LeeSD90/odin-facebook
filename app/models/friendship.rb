4def51d9cdf32ea963e8a240b647d189773c8a07
LeeSD90 <lee.doyle90@gmail.com>
2018-05-15 13:55:47 +0200

    Add inverse friendship creation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,27 @@ class Friendship < ApplicationRecord
     belongs_to  :user
     belongs_to  :friend, :class_name => "User"
 
+    after_create    :create_inverse, unless: :has_inverse?
+    after_destroy   :destroy_inverses, if: :has_inverse?
+
     validates   :user_id, presence: true
     validates   :friend_id, presence: true
+
+    private
+
+        def create_inverse
+            self.class.create(inverse_match_options)
+        end
+
+        def destroy_inverse
+            self.class.where(inverse_match_options).destroy_all
+        end
+
+        def has_inverse?
+            self.class.exists?(inverse_match_options)
+        end
+
+        def inverse_match_options
+            { user_id: friend_id, friend_id: user_id }
+        end
 end

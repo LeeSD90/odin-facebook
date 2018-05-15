77671c6272bf4358e38569b70a5c0624ef159be9
LeeSD90 <lee.doyle90@gmail.com>
2018-05-15 13:32:53 +0200

    Set up basic friendship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,10 @@ user = User.new()
 user.email = 'test@example.com'
 user.password = 'password'
 user.save!
+
+user = User.new()
+user.email = 'test2@example.com'
+user.password = 'password'
+user.save!
+
+user.friendships.create(:friend_id => User.find_by(:email => "test@example.com").id)

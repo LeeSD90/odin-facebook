562defe8231b3cb417534f02e57b050184f50e47
LeeSD90 <lee.doyle90@gmail.com>
2018-05-16 19:18:32 +0200

    Add friend list pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+require 'will_paginate/array'
     before_action   :authenticate_user!
 
     def index
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        @users = @user.get_friends.paginate(:page => params[:page])
     end
 
 end

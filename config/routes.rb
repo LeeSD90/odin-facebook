79e1b72693054edf93f87d55d5a5e46825687d16
LeeSD90 <lee.doyle90@gmail.com>
2018-05-16 12:21:43 +0200

    Add name to users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    root to: "devise/sessions#new"
+  devise_scope  :user do
+    authenticated :user do
+      root  'users#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root  'devise/sessions#new', as: :unauthenticated_root
+    end
   end
-  devise_for :users
+  root  'devise/sessions#new'
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, :only => [:index, :show]
   resources :friendships, only: [:create, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 user = User.new()
 user.email = 'test@example.com'
 user.password = 'password'
+user.name = 'Joe'
 user.save!
 
 user = User.new()
 user.email = 'test2@example.com'
 user.password = 'password'
+user.name = 'Bob'
 user.save!
 
 #user.friendships.create(:friend_id => User.find_by(:email => "test@example.com").id)

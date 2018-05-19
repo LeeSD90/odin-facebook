2b058f6579af4974350e6b7c4bde962737ef89ca
LeeSD90 <lee.doyle90@gmail.com>
2018-05-19 12:40:11 +0200

    Add gravatar to profile display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,13 @@
 module UsersHelper
+
     def current_user?(user)
         current_user == user
     end
+
+    def gravatar_for(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
+
 end
